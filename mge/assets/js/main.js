c5072477612d7582861f2a0be7e118bff63e1175
qkohst <kukohsantoso013@gmail.com>
2023-01-31 18:03:13 +0700

    Add About Page MGE

--- mge/assets/js/main.js
+++ mge/assets/js/main.js
@@ -282,6 +282,52 @@
   });
 
   /**
+   * About  slider
+   */
+  new Swiper('.about-slider', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  });
+
+  /**
+   * Day in slider
+   */
+  new Swiper('.day-in-slider', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        // spaceBetween: 40
+      },
+
+      1200: {
+        slidesPerView: 1.7,
+      }
+    }
+  });
+
+  /**
    * Animation on scroll
    */
   function aos_init() {
